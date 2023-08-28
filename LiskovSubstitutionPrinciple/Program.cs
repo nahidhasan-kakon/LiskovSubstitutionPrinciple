43562b36a9787a50aa90ccdd9c88be26fc451a41
Nahid Hasan Kakon <nahid@nuarca.com>
2023-08-28 14:40:36 +0600

    Add project files.

---
+++ LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,36 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bird sparrow= new Sparrow();
+        sparrow.Fly();
+
+        Bird penguin = new Penguin();
+        penguin.Fly();
+    }
+}
+
+public class Bird
+{
+    public virtual void Fly()
+    {
+        Console.WriteLine("Bird fly");
+    }
+}
+
+public class Sparrow : Bird
+{
+    public override void Fly()
+    {
+        Console.WriteLine("Sparrow fly");
+    }
+}
+
+public class Penguin : Bird
+{
+    public override void Fly()
+    {
+        Console.WriteLine("Penguin fly");
+    }
+}
